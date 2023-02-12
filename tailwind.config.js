2fcb6c7d97950c675daae237de5c36a1048bdf5e
Astern2112 <sternaarond@gmail.com>
2023-02-12 18:56:50 -0500

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,6 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      'torea-bay': {
-        50: '#f2f7ff',
-        100: '#e6eeff',
-        200: '#d1dfff',
-        300: '#acc3ff',
-        400: '#7e9eff',
-        500: '#496bff',
-        600: '#2441ff',
-        700: '#132dee',
-        800: '#0f25c8',
-        900: '#0f21a9',
-      },
-    },
-
     fontFamily: {
       sans: [
         '-apple-system',
@@ -32,7 +17,22 @@ module.exports = {
         'sans-serif',
       ],
     },
-    extend: {},
+    extend: {
+      colors: {
+        'torea-bay': {
+          50: '#f2f7ff',
+          100: '#e6eeff',
+          200: '#d1dfff',
+          300: '#acc3ff',
+          400: '#7e9eff',
+          500: '#496bff',
+          600: '#2441ff',
+          700: '#132dee',
+          800: '#0f25c8',
+          900: '#0f21a9',
+        },
+      },
+    },
   },
   plugins: [],
 };
